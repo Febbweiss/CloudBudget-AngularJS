7b44292c968e8e8cba2781154355d4203af096bc
febbweiss <fabrice.ecaille@gmail.com>
2015-10-01 14:13:10 +0000

    Clean: unused function

--- public/js/services/flash.service.js
+++ public/js/services/flash.service.js
@@ -13,26 +13,7 @@
         service.success = success;
         service.error = error;
         
-        initService();
-        
         return service;
-        
-        function initService() {
-            $rootScope.$on('$locationChangeStart', function() {
-                clearFlashMessage();
-            });
-            
-            function clearFlashMessage() {
-                var flash = $rootScope.flash;
-                if( flash ) {
-                    if( !flash.keepAfterLocationChange ) {
-                        delete $rootScope.flash;
-                    } else {
-                        flash.keepAfterLocationChange = false;
-                    }
-                }
-            }
-        }
         
         function success(message, keepAfterLocationChange) {
             growl.success(message,{title: 'Success!'});
